bd881b61b3c1b6de601cc3ea20f776b125619a0d
ojpro <contact@ojpro.me>
2024-02-10 20:47:20 +0100

    initial project setup.

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+        <main className="w-full min-h-screen">
+            <div className="text-center">Pomodoro</div>
+        </main>
+    </>
+  );
+}
