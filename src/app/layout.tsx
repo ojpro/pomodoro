8a6c4fb909a4a70e1ffbdc46ed85133d87add90a
ojpro <contact@ojpro.me>
2024-02-19 00:41:13 +0100

    feat: changed favicons, title and description.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,20 @@ import StoreProvider from "@/helpers/StoreProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Pomodoro App",
-    description: "Made for people that what experience best experience",
+    title: "Pomodoro Pulse: Maximize Your Productivity",
+    description: "Designed for Pomodoro enthusiasts aiming for maximum productivity.",
 };
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <StoreProvider>
             <html lang="en">
+                <head>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                </head>
                 <body>
                     {children}
                 </body>
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -18,7 +18,7 @@ export default function MainView() {
 
     return (<>
         <NextUIProvider>
-            <main className="w-full min-h-screen bg-[url(/images/bg-nature-6.jpg)] bg-no-repeat bg-cover bg-gray-200 relative">
+            <main className="min-w-[100svw] overflow-x-hidden min-h-svh bg-[url(/images/bg-nature-6.jpg)] bg-no-repeat bg-cover bg-gray-200 relative">
 
                 <header className='relative'>
                     {/* Sessions Switcher */}
