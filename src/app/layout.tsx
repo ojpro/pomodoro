0010f9913cb47bd2eb8368bec2a49bbb264c1655
ojpro <contact@ojpro.me>
2024-02-19 18:28:40 +0100

    ref: changed open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png" />
+                    <meta name="author" content="Oussama ELJABBARI" />
                     <link rel="manifest" href="/site.webmanifest" />
 
                     {/* Facebook Meta Tags */}
@@ -24,7 +25,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     <meta property="og:type" content="website" />
                     <meta property="og:title" content="Pomodoro Pulse: Maximize Your Productivity" />
                     <meta property="og:description" content="Designed for Pomodoro enthusiasts aiming for maximum productivity." />
-                    <meta property="og:image" content="/images/ogimage.png" />
+                    <meta property="og:image" content="/images/ogimage.jpg" />
 
                     {/* Twitter Meta Tags  */}
                     <meta name="twitter:card" content="summary_large_image" />
@@ -32,7 +33,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     <meta property="twitter:url" content="https://pomodoropulse.com/" />
                     <meta name="twitter:title" content="Pomodoro Pulse: Maximize Your Productivity" />
                     <meta name="twitter:description" content="Designed for Pomodoro enthusiasts aiming for maximum productivity." />
-                    <meta name="twitter:image" content="/images/ogimage.png" />
+                    <meta name="twitter:image" content="/images/ogimage.jpg" />
 
                 </head>
                 <body>
